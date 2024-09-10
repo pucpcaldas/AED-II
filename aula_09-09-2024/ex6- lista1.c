448d26c46490e8fc5a385da1ab2345b072e1a0b5
Davi Campos <davicampos.dev@gmail.com>
2024-09-10 18:51:34 -0300

    Rename parent folder

---
+++ aula_09-09-2024/ex6- lista1.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int ff(int n);
+int main() {
+    int r;
+    r = ff(7);
+    printf("%d", r);    
+    return 0;
+}
+int ff(int n) {
+    if (n == 1) return 1;
+    if (n % 2 == 0) return ff(n/2);
+    return ff((n-1)/2) + ff((n+1)/2);
+}
